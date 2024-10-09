8a7f6d0a71b12b2b750f49a61720d75a7c1c49db
Lucas Safeno <lugnorio@gmail.com>
2024-10-09 22:45:14 +0000

    ##46:44min Laravel NWL

---
+++ freelance-hours/resources/views/projects/index.blade.php
@@ -0,0 +1,3 @@
+<a href="{{route('project.show', 1)}}">
+  Primeiro projeto
+</a>
--- freelance-hours/routes/web.php
+++ freelance-hours/routes/web.php
@@ -2,6 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view('/', 'projects.index')->name('project.index');
+
+Route::view('/project/{project}', 'projects.show')->name('project.show');
